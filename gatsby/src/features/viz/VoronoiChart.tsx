7e670b719701c0be440dd75327c32edcb3fd6504
Philip Hradecsni <ph@webundsoehne.com>
2021-11-28 16:43:20 +0100

    add project layout calculation

--- gatsby/src/features/viz/VoronoiChart.tsx
+++ gatsby/src/features/viz/VoronoiChart.tsx
@@ -3,6 +3,7 @@ import { UseCanvas2DDrawFn, useCanvas2D } from '../../common/hooks/useCanvas'
 import { useAsyncEffect } from '@react-hook/async'
 import { useDebounce } from '@react-hook/debounce'
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import chunk from 'lodash.chunk'
 import useSize from '@react-hook/size'
 
 type VoronoiChartProps = {
@@ -26,17 +27,13 @@ export const VoronoiChart = ({ data, margin = 0 }: VoronoiChartProps) => {
     cancel,
     error,
     value: images
-  } = useAsyncEffect(() => Promise.all(data.map(({ imageUrl }) => loadImage(imageUrl))), [])
+  } = useAsyncEffect(() => Promise.all(data.map(({ imageUrl }) => (imageUrl ? loadImage(imageUrl) : null))), [])
 
-  useEffect(() => {
-    console.log(status, error, images)
-  })
+  const particles = useMemo(() => data.map(({ x, y }) => [x * width, y * height]), [data, width, height])
 
   useEffect(() => {
     setDimensions([liveWidth, liveHeight])
   }, [liveHeight, liveWidth, setDimensions])
-
-  const particles = useMemo(() => data.map(({ x, y }) => [x * width, y * height]), [data, width, height])
 
   const memoOptions = useMemo(
     () => ({
@@ -46,63 +43,68 @@ export const VoronoiChart = ({ data, margin = 0 }: VoronoiChartProps) => {
     [height, width]
   )
 
+  useEffect(() => {
+    console.log(status, images)
+  }, [images, status])
+
+  useEffect(() => {
+    console.log('particles', particles)
+    console.log('memoOptions', memoOptions)
+  }, [particles, memoOptions])
+
   const canvasRef = useCanvas2D(draw(particles, { strokeStyle, images }), memoOptions)
 
   return (
     <div ref={chartRef} className="h-full w-full">
-      <canvas ref={canvasRef} />
+      {width && height && <canvas ref={canvasRef} />}
     </div>
   )
 }
 
+function clippingPath(polygon: number[][], img: HTMLImageElement, x: number, y: number, ctx) {
+  const minSize = 600
+  // save the unclipped context
+  ctx.save()
+
+  // define the path that will be clipped to
+  ctx.beginPath()
+  ctx.moveTo(polygon[0][0], polygon[0][1])
+  for (let i = 0; i < polygon.length; i++) {
+    ctx.lineTo(polygon[i][0], polygon[i][1])
+  }
+  ctx.closePath()
+
+  // stroke the path
+  // half of the stroke is outside the path
+  // the outside stroke will survive the clipping that follows
+  ctx.lineWidth = 2
+  ctx.stroke()
+
+  // make the current path a clipping path
+  ctx.clip()
+
+  // draw the image which will be clipped except in the clipping path
+  const aspectRatio = img.width / img.height
+  const isLandscape = aspectRatio > 1
+  const baseSize = Math.min(img.width, img.height, minSize)
+  const imgWidth = isLandscape ? baseSize * aspectRatio : baseSize
+  const imgHeight = isLandscape ? baseSize : baseSize / aspectRatio
+  ctx.drawImage(img, x - imgWidth / 2, y - imgHeight / 2, imgWidth, imgHeight)
+
+  // restore the unclipped context (==undo the clipping path)
+  ctx.restore()
+}
 const draw =
   (particles, { strokeStyle, images }): UseCanvas2DDrawFn =>
   (ctx) => {
     const width = ctx.canvas.width
     const height = ctx.canvas.height
 
-    function clippingPath(polygon, img, x, y) {
-      // save the unclipped context
-      ctx.save()
-
-      // define the path that will be clipped to
-      ctx.beginPath()
-      ctx.moveTo(polygon[0][0], polygon[0][1])
-      for (let i = 0; i < polygon.length; i++) {
-        console.log(polygon[i][0], polygon[i][1])
-        ctx.lineTo(polygon[i][0], polygon[i][1])
-      }
-      ctx.closePath()
-
-      // stroke the path
-      // half of the stroke is outside the path
-      // the outside stroke will survive the clipping that follows
-      ctx.lineWidth = 2
-      ctx.stroke()
-
-      // make the current path a clipping path
-      ctx.clip()
-
-      // draw the image which will be clipped except in the clipping path
-      ctx.drawImage(img, x, y)
-
-      // restore the unclipped context (==undo the clipping path)
-      ctx.restore()
-    }
-
     function update() {
       const delaunay = d3.Delaunay.from(particles)
 
-      const voronoi = delaunay.voronoi([0.5, 0.5, width - 0.5, height - 0.5])
+      const voronoi = delaunay.voronoi([-1, -1, width + 1, height + 1])
       ctx.clearRect(0, 0, width, height)
-
-      const polygons = Array.from(voronoi.cellPolygons())
-
-      if (images) {
-        polygons.forEach((polygon, i) => {
-          clippingPath(polygon, images[i], i * 100, i * 80)
-        })
-      }
 
       ctx.beginPath()
       delaunay.render(ctx)
@@ -118,6 +120,15 @@ const draw =
       ctx.beginPath()
       delaunay.renderPoints(ctx, 2)
       ctx.fill()
+
+      const centers = Array.from(delaunay.points)
+      const polygons = Array.from(voronoi.cellPolygons())
+      const bundle = d3.transpose([images, polygons, chunk(centers, 2)])
+      for (const [image, polygon, center] of bundle) {
+        if (image) {
+          clippingPath(polygon, image, center[0], center[1], ctx)
+        }
+      }
     }
 
     ctx.canvas.ontouchmove = ctx.canvas.onmousemove = (event) => {
--- gatsby/src/pages/projects.tsx
+++ gatsby/src/pages/projects.tsx
@@ -1,3 +1,4 @@
+import * as d3 from 'd3'
 import { AllProjectsDataQuery } from '../../graphql-types'
 import { VoronoiChart } from '../features/viz/VoronoiChart'
 import { graphql } from 'gatsby'
@@ -5,15 +6,28 @@ import Layout from '../common/components/Layout'
 import React, { FC, useMemo } from 'react'
 
 const ProjectsPage: FC<{ data: AllProjectsDataQuery }> = ({ data: { allStrapiProject } }) => {
-  const projects = allStrapiProject.edges.map((edge) => edge.node)
+  const projects = d3.shuffle([...allStrapiProject.edges.map((edge) => edge.node), 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+  const rows = 4
+  const columns = 4
+  const imagePositions = projects.map((_, idx) => {
+    const colIdx = idx % columns
+    const rowIdx = Math.floor(idx / columns)
+    return [
+      d3.range(0.2, 0.8, 0.6 / Math.max(columns - 1, 1))[colIdx],
+      d3.range(0.2, 0.8, 0.6 / Math.max(rows - 1, 1))[rowIdx]
+    ]
+  })
+  console.log({ imagePositions, projects })
   const chartData = useMemo(
     () =>
-      projects.map(({ images }) => ({
-        x: Math.random(),
-        y: Math.random(),
-        imageUrl: process.env.GATSBY_API_URL + images[0].url
+      projects.map(({ images }, idx) => ({
+        x: imagePositions[idx][0] + d3.randomNormal(0, 0.03)(),
+        y: imagePositions[idx][1] + d3.randomNormal(0, 0.03)(),
+        imageUrl: images
+          ? process.env.GATSBY_API_URL + images[0].url
+          : 'https://picsum.photos/600/' + (idx % 2 ? '500' : '600')
       })),
-    [projects]
+    [imagePositions, projects]
   )
 
   console.log(chartData)
