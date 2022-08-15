1f1f0a17c2b1c39dce64fecb4d2d5ba65b88f7f9
Philip Hradecsni <ph@webundsoehne.com>
2022-08-15 16:57:00 +0200

    trigger deployment

--- src/features/viz/Voronoi/VoronoiChart.tsx
+++ src/features/viz/Voronoi/VoronoiChart.tsx
@@ -1,11 +1,13 @@
 import * as d3 from 'd3-selection'
-import React, { memo, SVGProps, useCallback, useEffect, useMemo, useState } from 'react'
+import React, { memo, SVGProps, useCallback, useEffect, useMemo, useRef, useState } from 'react'
 
 import './voronoi.scss'
 import { VoronoiChartDatum } from '../../projects/use-projects-chart-data'
 import { drawVoronoi } from './helpers/draw-voronoi'
 import { highlightCellsByAreaId, hoverCell, restore, VoronoiOptions } from './helpers/voronoi-actions'
 import { useVoronoiModel } from './use-voronoi-model'
+
+import { Link } from 'gatsby'
 
 type HighlightPatternDatum = { color?: string | null; id: string | number }
 
@@ -31,10 +33,13 @@ export const VoronoiChart = memo(
   }: VoronoiChartProps) => {
     const [svgNode, setSvgNode] = useState<SVGSVGElement | null>(null)
     const [initialized, setInitialized] = useState(false)
+    const [hoveredCell, setHoveredCell] = useState('')
 
     const onRefChange = useCallback((node: SVGSVGElement | null) => {
       setSvgNode(node)
     }, [])
+
+    const spanRef = useRef<HTMLAnchorElement>(null)
 
     const { voronoi, enrichedData } = useVoronoiModel({ data, width, height })
 
@@ -61,7 +66,14 @@ export const VoronoiChart = memo(
           data: enrichedData,
           options: voronoiOptions,
           onClick: onClickCell,
-          onHover: hoverCell(svgNode),
+          onHover: (slug) => {
+            if (spanRef.current) {
+              setHoveredCell(slug)
+              const mouseOverEvent = new MouseEvent('mouseover', { bubbles: true })
+              spanRef.current?.dispatchEvent(mouseOverEvent)
+            }
+            hoverCell(svgNode)(slug)
+          },
           onMouseLeave: restore(svgNode)
         })
         setInitialized(true)
@@ -75,14 +87,17 @@ export const VoronoiChart = memo(
     }, [highlightedAreaId, initialized, svgNode])
 
     return (
-      <svg ref={onRefChange} width={width} height={height} className="cursor-pointer voronoi animate-fadeIn">
-        <defs>
-          <HatchPattern id="diagonalHatch" className="stroke-bg-secondary" />
-          {highlightPatternData.map(({ color, id }) => (
-            <HatchPattern key={id} stroke={color || 'black'} id={'diagonalHatchHighlight-' + id} />
-          ))}
-        </defs>
-      </svg>
+      <>
+        <Link ref={spanRef as any} key={hoveredCell} to={hoveredCell} style={{ display: 'none' }} />
+        <svg ref={onRefChange} width={width} height={height} className="cursor-pointer voronoi animate-fadeIn">
+          <defs>
+            <HatchPattern id="diagonalHatch" className="stroke-bg-secondary" />
+            {highlightPatternData.map(({ color, id }) => (
+              <HatchPattern key={id} stroke={color || 'black'} id={'diagonalHatchHighlight-' + id} />
+            ))}
+          </defs>
+        </svg>
+      </>
     )
   }
 )
